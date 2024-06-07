1268450b9ef84bd9ce244923a22e4d0afd65a073
fihra <fabrofabian@gmail.com>
2024-06-06 19:27:53 -1000

    Declare Display stats

--- Battle.h
+++ Battle.h
@@ -5,12 +5,15 @@
 #include "Player.h";
 #include "Enemy.h";
 #include <memory>
+#include <vector>
 
 class Battle
 {
 public:
 	void Attacking(std::unique_ptr<Player>& player, std::unique_ptr<Enemy>& enemy);
 	void Defending(std::unique_ptr<Enemy>& enemy, std::unique_ptr<Player>& player);
+	void DisplayPlayerStats(std::unique_ptr<Player>& player, std::vector<int> stats);
+	void DisplayEnemyStats(std::unique_ptr<Enemy>& enemy, std::vector<int> stats);
 };
 
 #endif
