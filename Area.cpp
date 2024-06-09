d566c840875f6e89b76e1fe0220efe8b1772842f
fihra <fabrofabian@gmail.com>
2024-06-08 23:33:58 -1000

    Created map area class

---
+++ Area.cpp
@@ -0,0 +1,44 @@
+﻿#include "Area.h"
+
+void Area::MeadowMap()
+{
+	std::cout << "=======================\n";
+	std::cout << "Entering the Meadows\n";
+	std::cout << "=======================\n";
+	system("pause");
+	std::cout << "Strolling across the flat grass~\n";
+}
+
+void Area::ForestMap()
+{
+	std::cout << "=======================\n";
+	std::cout << "Entering the Forest\n";
+	std::cout << "=======================\n";
+	system("pause");
+	std::cout << "Treading on the path with surrounding trees.\n";
+}
+
+void Area::MountainMap()
+{
+	std::cout << "=======================\n";
+	std::cout << "Entering the Mountain\n";
+	std::cout << "=======================\n";
+	system("pause");
+	std::cout << "Climbing the path up the mountain.\n";
+}
+
+void Area::CastleMap()
+{
+	std::cout << "=======================\n";
+	std::cout << "Entering the Castle\n";
+	std::cout << "=======================\n";
+	system("pause");
+}
+
+void Area::RestMap()
+{
+	std::cout << "=======================\n";
+	std::cout << "Rest stop\n";
+	std::cout << "=======================\n";
+	system("pause");
+}
